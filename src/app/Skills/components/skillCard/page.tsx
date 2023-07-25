a902010bbb6c737835bf77e24c634a47ce358b11
MS07062000 <singhmurali.14@gmail.com>
2023-07-25 21:42:54 +0530

    skillcard error solved

--- src/app/Skills/components/skillCard/page.tsx
+++ src/app/Skills/components/skillCard/page.tsx
@@ -2,22 +2,38 @@ import React, { FunctionComponent } from "react";
 import styles from './style.module.css';
 import Image, { StaticImageData } from 'next/image';
 
-type SkillsProps = { skillName: string; skillIconImage: string | StaticImageData }
+type SkillsProps = { skillName: string; skillIconImage: string|StaticImageData }
 
 const SkillCard: FunctionComponent<SkillsProps> = ({ skillName, skillIconImage }) => {
-    return (
-        <div className={styles.card}>
-            <Image
-                className={styles.skillImage}
-                src={skillIconImage}
-                width={50}
-                height={50}
-                alt="skillLogo"
-            />
-            <p className={styles.skillName}>{skillName}</p>
-        </div>
-    );
+    const isUrl = typeof skillIconImage === "string";
 
+    if (isUrl) {
+        return (
+            <div className={styles.card}>
+                <Image
+                    className={styles.skillImage}
+                    src={skillIconImage}
+                    width={50}
+                    height={50}
+                    alt="skillLogo"
+                />
+                <p className={styles.skillName}>{skillName}</p>
+            </div>
+        );
+    } else {
+        return (
+            <div className={styles.card}>
+                <Image
+                    className={styles.skillImage}
+                    src={skillIconImage}
+                    width={50}
+                    height={50}
+                    alt="skillLogo"
+                />
+                <p className={styles.skillName}>{skillName}</p>
+            </div>
+        );
+    }
 }
 
 
